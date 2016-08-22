4280539f7956476e9ca043ffcdfaac08ffaaa41f
wrmsr <timwilloney@gmail.com>
2016-08-21 22:11:51 -0700

    Initial

---
+++ carlae/__about__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+
+__title__ = 'carlae'
+__version__ = '0.1'
+__description__ = 'Carlae utility library'
+__author__ = 'wrmsr'
+__url__ = 'https://github.com/wrmsr/carlae'
+__license__ = 'MIT'
